95a113332a35845e2ca08e0806fea1d0cc057dc5
DreamHackchosenone <15521370865@163.com>
2017-10-03 02:26:37 +0800

    second

--- crawlIP/pipelines.py
+++ crawlIP/pipelines.py
@@ -17,22 +17,14 @@ class CrawlipPipeline(object):
         try:
             requests.get('http://wenshu.court.gov.cn/', proxies={"http":str(item['ip']).lstrip('[\'').rstrip('\']')})
         except:
-            log.msg('*'*10,level=log.INFO)
-            log.msg('*' * 10, level=log.INFO)
-            log.msg('*' * 10, level=log.INFO)
+
             log.msg(str(item['ip'])+'is not useful', level=log.INFO)
-            log.msg('*' * 10, level=log.INFO)
-            log.msg('*' * 10, level=log.INFO)
-            log.msg('*' * 10, level=log.INFO)
         else:
-            log.msg('-'*10,level=log.INFO)
-            log.msg('-' * 10, level=log.INFO)
-            log.msg('-' * 10, level=log.INFO)
-            log.msg('-' * 10, level=log.INFO)
             proxyip = proxyIP(ip=str(item['ip']).lstrip('[\'').rstrip('\']'),
                               port=str(item['port']).strip('[\'').rstrip('\']'),
                               high_hide=str(item['high_hide']).strip('[\'').rstrip('\']'),
                               type=str(item['type']).strip('[\'').rstrip('\']'))
+            log.msg(str(item['ip'])+'is useful,already add in database', level=log.INFO)
             self.session.add(proxyip)
             self.session.commit()
         finally:
