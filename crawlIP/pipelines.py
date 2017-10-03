708ea2b04718a3b8d3f307ab1f599fc9cd71cade
DreamHackchosenone <15521370865@163.com>
2017-10-04 03:09:30 +0800

    third

--- crawlIP/pipelines.py
+++ crawlIP/pipelines.py
@@ -13,21 +13,24 @@ import requests
 class CrawlipPipeline(object):
     def open_spider(self, spider):
         self.session = Session()
+
     def process_item(self, item, spider):
         try:
-            requests.get('http://wenshu.court.gov.cn/', proxies={"http":str(item['ip']).lstrip('[\'').rstrip('\']')})
+            requests.get('https://www.baidu.com/', proxies={"http": str(item['ip']).lstrip('[\'').rstrip('\']')},
+                         timeout=3)
         except:
 
-            log.msg(str(item['ip'])+'is not useful', level=log.INFO)
+            log.msg(str(item['ip']) + 'is not useful', level=log.INFO)
         else:
             proxyip = proxyIP(ip=str(item['ip']).lstrip('[\'').rstrip('\']'),
                               port=str(item['port']).strip('[\'').rstrip('\']'),
                               high_hide=str(item['high_hide']).strip('[\'').rstrip('\']'),
                               type=str(item['type']).strip('[\'').rstrip('\']'))
-            log.msg(str(item['ip'])+'is useful,already add in database', level=log.INFO)
+            log.msg(str(item['ip']) + 'is useful,already add in database', level=log.INFO)
             self.session.add(proxyip)
             self.session.commit()
         finally:
             return item
-    def close_spider(self,spider):
+
+    def close_spider(self, spider):
         self.session.close()
